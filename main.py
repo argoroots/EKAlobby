a409e68d7a0f85fe7281b75eb900058ae893478a
Argo Roots <argo@roots.ee>
2013-10-11 16:38:02 +0300

    Error handling

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import rfc822
 import xmltodict
 import json
 import webapp2
+import logging
 
 
 rooms_url = 'https://eka.entu.ee/api/get_entity_list?only_public=true&full_info=true&entity_definition_keyname=room'
@@ -29,37 +30,40 @@ class GetRoomsCalendar(webapp2.RequestHandler):
     def get(self, room=''):
         room = room.strip('/')
         calendars = []
-        rooms = json.loads(urlfetch.fetch(rooms_url, deadline=100).content)
-        for r in rooms:
-            if r.get('displayname')[:len(room)].upper() != room.upper():
-                continue
-            if not r.get('properties', {}).get('calendar', {}).get('values'):
-                continue
-            for v in r.get('properties', {}).get('calendar', {}).get('values'):
-                if not v.get('value'):
+        try:
+            rooms = json.loads(urlfetch.fetch(rooms_url, deadline=100).content)
+            for r in rooms:
+                if r.get('displayname')[:len(room)].upper() != room.upper():
                     continue
-                ical_file = urlfetch.fetch(v.get('value'), deadline=100).content
-                try:
-                    ical = Calendar.from_ical(ical_file)
-                except Exception, e:
+                if not r.get('properties', {}).get('calendar', {}).get('values'):
                     continue
-                calendar = []
-                for c in ical.walk():
-                    if c.name != "VEVENT":
+                for v in r.get('properties', {}).get('calendar', {}).get('values'):
+                    if not v.get('value'):
                         continue
-                    if _get_time(c.get('dtstart').dt).date() > date.today():
+                    ical_file = urlfetch.fetch(v.get('value'), deadline=100).content
+                    try:
+                        ical = Calendar.from_ical(ical_file)
+                    except Exception, e:
                         continue
-                    if _get_time(c.get('dtend').dt).date() < date.today():
-                        continue
-                    calendars.append({
-                        'name': r.get('displayname'),
-                        'info': r.get('displayinfo'),
-                        'start': str(_get_time(c.get('dtstart').dt)),
-                        'end': str(_get_time(c.get('dtend').dt)),
-                        'summary': c.get('summary'),
-                        # 'description': c.get('description'),
-                    })
-        calendars = sorted(calendars, key=itemgetter('start', 'name', 'info'))
+                    calendar = []
+                    for c in ical.walk():
+                        if c.name != "VEVENT":
+                            continue
+                        if _get_time(c.get('dtstart').dt).date() > date.today():
+                            continue
+                        if _get_time(c.get('dtend').dt).date() < date.today():
+                            continue
+                        calendars.append({
+                            'name': r.get('displayname'),
+                            'info': r.get('displayinfo'),
+                            'start': str(_get_time(c.get('dtstart').dt)),
+                            'end': str(_get_time(c.get('dtend').dt)),
+                            'summary': c.get('summary'),
+                            # 'description': c.get('description'),
+                        })
+            calendars = sorted(calendars, key=itemgetter('start', 'name', 'info'))
+        except Exception, e:
+            logging.error(e)
 
         self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
         self.response.write(json.dumps(calendars))
@@ -68,13 +72,16 @@ class GetRoomsCalendar(webapp2.RequestHandler):
 class GetNews(webapp2.RequestHandler):
     def get(self):
         news = []
-        for n in xmltodict.parse(urlfetch.fetch(news_url, deadline=100).content).get('rss', {}).get('channel', {}).get('item'):
-            news.append({
-                'date': str(_get_time(datetime.fromtimestamp(mktime(rfc822.parsedate(n.get('pubDate')))))),
-                'title': n.get('title'),
-                'description': n.get('description'),
-            })
-        news = sorted(news, key=itemgetter('date', 'title'))
+        try:
+            for n in xmltodict.parse(urlfetch.fetch(news_url, deadline=100).content).get('rss', {}).get('channel', {}).get('item'):
+                news.append({
+                    'date': str(_get_time(datetime.fromtimestamp(mktime(rfc822.parsedate(n.get('pubDate')))))),
+                    'title': n.get('title'),
+                    'description': n.get('description'),
+                })
+            news = sorted(news, key=itemgetter('date', 'title'))
+        except Exception, e:
+            logging.error(e)
 
         self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
         self.response.write(json.dumps(news))
