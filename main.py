bfa665f9ee55939e63209bbf127d5e8e49fdd49c
Argo Roots <argo@roots.ee>
2014-04-19 15:38:25 +0300

    Reenabled commented code

--- main.py
+++ main.py
@@ -70,53 +70,53 @@ class FillMemcache(webapp2.RequestHandler):
         news = []
         events = []
 
-        # try:
-        #     for n in xmltodict.parse(urlfetch.fetch(news_url, deadline=100).content).get('rss', {}).get('channel', {}).get('item'):
-        #         news.append({
-        #             'date': _get_time(datetime.fromtimestamp(mktime(rfc822.parsedate(n.get('pubDate'))))),
-        #             'title': n.get('title'),
-        #             'text': n.get('description'),
-        #             'link': n.get('link'),
-        #         })
-        #     _set_cache(key='news', value=news)
-        #     logging.info('News added to DB: %s' % len(news))
-        # except Exception, e:
-        #     logging.error('News import: ' % e)
+        try:
+            for n in xmltodict.parse(urlfetch.fetch(news_url, deadline=100).content).get('rss', {}).get('channel', {}).get('item'):
+                news.append({
+                    'date': _get_time(datetime.fromtimestamp(mktime(rfc822.parsedate(n.get('pubDate'))))),
+                    'title': n.get('title'),
+                    'text': n.get('description'),
+                    'link': n.get('link'),
+                })
+            _set_cache(key='news', value=news)
+            logging.info('News added to DB: %s' % len(news))
+        except Exception, e:
+            logging.error('News import: ' % e)
 
-#        try:
-        rooms = json.loads(urlfetch.fetch(rooms_url, deadline=100).content)
-        logging.info('Started to load %s rooms' % len(rooms))
-        for idx, r in enumerate(rooms):
-            if not r.get('properties', {}).get('calendar', {}).get('values'):
-                logging.warning('#%s - %s - No iCal property' % (idx, r.get('displayname')))
-                continue
-            for v in r.get('properties', {}).get('calendar', {}).get('values'):
-                if not v.get('value'):
-                    logging.warning('#%s - %s - No iCal URL' % (idx, r.get('displayname')))
+       try:
+            rooms = json.loads(urlfetch.fetch(rooms_url, deadline=100).content)
+            logging.info('Started to load %s rooms' % len(rooms))
+            for idx, r in enumerate(rooms):
+                if not r.get('properties', {}).get('calendar', {}).get('values'):
+                    logging.warning('#%s - %s - No iCal property' % (idx, r.get('displayname')))
                     continue
-                try:
-                    ical_file = urlfetch.fetch(v.get('value'), deadline=100).content
-                    for component in vobject.readComponents(ical_file):
-                        for event in component.vevent_list:
-                            if _get_time(event.dtend.value).date() < _get_time(datetime.today()).date():
-                                continue
+                for v in r.get('properties', {}).get('calendar', {}).get('values'):
+                    if not v.get('value'):
+                        logging.warning('#%s - %s - No iCal URL' % (idx, r.get('displayname')))
+                        continue
+                    try:
+                        ical_file = urlfetch.fetch(v.get('value'), deadline=100).content
+                        for component in vobject.readComponents(ical_file):
+                            for event in component.vevent_list:
+                                if _get_time(event.dtend.value).date() < _get_time(datetime.today()).date():
+                                    continue
 
-                            events.append({
-                                'room': r.get('displayname'),
-                                'info': r.get('displayinfo'),
-                                'start': _get_time(event.dtstart.value),
-                                'end': _get_time(event.dtend.value),
-                                'summary': event.summary.value,
-                            })
+                                events.append({
+                                    'room': r.get('displayname'),
+                                    'info': r.get('displayinfo'),
+                                    'start': _get_time(event.dtstart.value),
+                                    'end': _get_time(event.dtend.value),
+                                    'summary': event.summary.value,
+                                })
 
-                    logging.info('#%s - %s - OK' % (idx, r.get('displayname')))
-                except Exception, e:
-                    logging.warning('#%s - %s - Cant open %s (%s)' % (idx, r.get('displayname'), v.get('value'), e))
-                    continue
-        _set_cache(key='events', value=events)
-        logging.info('Events added to DB: %s' % len(events))
-#        except Exception, e:
-#            logging.error('Event import: %s' % e)
+                        logging.info('#%s - %s - OK' % (idx, r.get('displayname')))
+                    except Exception, e:
+                        logging.warning('#%s - %s - Cant open %s (%s)' % (idx, r.get('displayname'), v.get('value'), e))
+                        continue
+            _set_cache(key='events', value=events)
+            logging.info('Events added to DB: %s' % len(events))
+       except Exception, e:
+           logging.error('Event import: %s' % e)
 
 
 def _get_time(t):
