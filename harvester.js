a66566dc52c9cc0e37f980b12d4dfba851afa06f
Argo Roots <argo@roots.ee>
2017-03-21 05:13:01 +0200

    Path fix

--- harvester.js
+++ harvester.js
@@ -64,7 +64,7 @@ var doHarvest = () => {
 
         sortedNews = _.sortBy(news, ['date'])
 
-        fs.writeFile('./json/news.json', JSON.stringify(sortedNews, null, 3), 'utf8', () => {
+        fs.writeFile(path.resolve(__dirname, 'json', 'news.json'), JSON.stringify(sortedNews, null, 3), 'utf8', () => {
             console.log((new Date()).toISOString(), `News: ${sortedNews.length}`);
         })
     })
@@ -100,7 +100,7 @@ var doHarvest = () => {
 
             sortedEvents = _.sortBy(events, ['start', 'end', 'title'])
 
-            fs.writeFile('./json/events.json', JSON.stringify(sortedEvents, null, 3), 'utf8', () => {
+            fs.writeFile(path.resolve(__dirname, 'json', 'events.json'), JSON.stringify(sortedEvents, null, 3), 'utf8', () => {
                 console.log((new Date()).toISOString(), `Events: ${sortedEvents.length}`)
             })
         })
